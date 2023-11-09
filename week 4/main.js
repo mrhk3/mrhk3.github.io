dd0a30b1099db46f828f5bf194179fe13c153e6f
mrhk3 <145095736+mrhk3@users.noreply.github.com>
2023-11-09 15:08:14 +0400

    updated

--- week 4/main.js
+++ week 4/main.js
@@ -2,7 +2,7 @@ var cityContainer = document.getElementById("city-info");
 var btn = document.getElementById("btn");
 btn.addEventListener("click", function(){
 var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://mrhk3.github.io/mrhk3.github.io/week 4/cities1.json');
+ourRequest.open('GET', 'https://mrhk3.github.io/week 4/cities1.json');
 ourRequest.onload = function() {
 var ourData = JSON.parse(ourRequest.responseText);
 renderHTML(ourData);

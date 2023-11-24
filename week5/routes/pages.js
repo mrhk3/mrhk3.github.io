ffd8e46348fa0ae9df7ee3c8c923187e8accde8b
mrhk3 <145095736+mrhk3@users.noreply.github.com>
2023-11-24 15:42:51 +0400

    week 5 updated

---
+++ week5/routes/pages.js
@@ -0,0 +1,50 @@
+// const express = require ('express');
+// const router =express.Router();
+// authController = require ('../controllers/auth');
+
+// router.get ('/', (req,res) => {
+//    res.render('index',{
+//     user: req.user
+//    });
+//  });
+
+//  router.get ('/register',(req,res) => {
+//      res.render("register");
+//     });
+
+//     router.get('/login',(req , res) =>{
+//       res.render("login");
+//     });
+
+//     router.get('/profile',(req,res) => {
+//       res.render("profile")
+//     });
+    
+// module.exports = router;
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/auth')
+
+
+router.get('/', (req, res) => {
+    res.render('index', {
+        user: req.user // Pass the user object to the template
+    });
+});
+router.get('/register', (req, res) => {
+    res.render('register');
+})
+
+router.get('/login', (req, res) => {
+    res.render('login');
+})
+router.get("/index",(req,res) =>{
+    res.render('index');
+});
+router.get('/profile', authController.isLoggedIn, (req, res) => {
+    console.log("this is the user from the req of the profile", req.user)
+    // Route logic for the profile page for authenticated users
+    res.render('profile', { user: req.user }); // req.user contains the user information
+});
+
+module.exports = router;
